bf9ccfdc4c3130af8d87b078ea4fb84fbf27c7a1
Vaibhav Sharma <yavsvaibhav@gmail.com>
2022-02-12 21:17:30 +0530

    changed to unable selling of stocks

--- Src/data_handling/scraper.py
+++ Src/data_handling/scraper.py
@@ -12,12 +12,6 @@ class ContUpdatedPrice:
     def __init__(self):
         driver.get('https://www.bseindia.com/')
 
-    # def resetVariables(self):
-    #     self.suggestions = None
-    #     self.unfiltered_list = []
-    #     self.suggestions_symbol_list = []
-    #     self.suggestions_company_list = []
-
     def setup(self, company_name: str):
         self.suggestions_empty = False
         self.left_out_suggestions_empty = False
@@ -30,13 +24,15 @@ class ContUpdatedPrice:
         search_box.send_keys(f'{company_name}')
         time.sleep(5)
         try:
+            self.suggestions = []
             self.suggestions = driver.find_elements_by_xpath("//li[@class='quotemenu']")
         except selenium.common.exceptions.NoSuchElementException:
             print("No Matches 1")
             self.suggestions_empty = True
         try:
+            self.left_out_suggestions = []
             self.left_out_suggestions = driver.find_elements_by_xpath("/ html / body / div / div[4] / div / div[3] / "
-                                                                     "div / section / form / div[1] / ul / li[1]")
+                                                                      "div / section / form / div[1] / ul / li[1]")
 
         except selenium.common.exceptions.NoSuchElementException:
             print("No Matches 2")
@@ -45,11 +41,13 @@ class ContUpdatedPrice:
         if not self.suggestions_empty:
             for value in self.suggestions:
                 if company_name.upper() in value.text:
+                    print(value.text)
                     self.unfiltered_list.append(value.text)
 
         if not self.left_out_suggestions_empty:
             for value in self.left_out_suggestions:
                 if company_name.upper() in value.text:
+                    print(value.text)
                     self.unfiltered_list.insert(0, value.text)
 
         if self.suggestions_empty and self.left_out_suggestions_empty:
@@ -63,19 +61,30 @@ class ContUpdatedPrice:
     def getSuggestions(self):
         return [self.suggestions_company_list, self.suggestions_symbol_list]
 
-    def selectAndClickSuggestions(self, index: int):
-        # print(self.suggestions_company_list)
-        to_search = self.suggestions_company_list[index]
-        # search_box = driver.find_element_by_xpath("//input[@id='getquotesearch']")
-        # search_box.clear()
-        # search_box.send_keys(f"{to_search}")
-        # click = driver.find_element_by_xpath(f"//*[contains(text(), '{to_search}')]")
-        # click.click()
-        link = "https://www.bseindia.com/stock-share-price/{0}/{1}/{2}"
-        unclean = self.suggestions_symbol_list[index].split(" ")
-        clean = [v for v in unclean if v != ""]
-        required_link = link.format(f"{to_search.replace(' ', '-').lower()}", f"{clean[0].lower()}", f"{clean[2]}")
-        driver.get(required_link)
+    def selectAndClickSuggestions(self, changeCriteria: bool, companyNameWithCode: str = '', index: int = 0):
+        if not changeCriteria:
+            # print(self.suggestions_company_list)
+            to_search = self.suggestions_company_list[index]
+            # search_box = driver.find_element_by_xpath("//input[@id='getquotesearch']")
+            # search_box.clear()
+            # search_box.send_keys(f"{to_search}")
+            # click = driver.find_element_by_xpath(f"//*[contains(text(), '{to_search}')]")
+            # click.click()
+            link = "https://www.bseindia.com/stock-share-price/{0}/{1}/{2}"
+            unclean = self.suggestions_symbol_list[index].split(" ")
+            clean = [v for v in unclean if v != ""]
+            required_link = link.format(f"{to_search.replace(' ', '-').lower()}", f"{clean[0].lower()}", f"{clean[2]}")
+            driver.get(required_link)
+
+        else:
+            to_search = companyNameWithCode
+            split = companyNameWithCode.split(' ')
+            companyName = split[0:-3]
+            digitCode = split[-1]
+            alphaCode = split[-2]
+            link = "https://www.bseindia.com/stock-share-price/{0}/{1}/{2}"
+            required_link = link.format(f"{companyName}", f"{alphaCode}", f"{digitCode}")
+            driver.get(required_link)
 
     def getUpdatedPrice(self):
         try:
